a8f3a48828599b589395c793059b64af7b869058
yakshina <yakshina_kristina@mail.ru>
2021-03-06 19:09:58 +0300

    fix: change functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  let y = [];
+  if ((array == undefined)||(array.length == y.length)){
+    return 0;
+  }
+  else {
+     let x = array[0];
+  array.forEach(item => {
+    if (item < x){
+      x = item;
+    }
+  });
+  return x;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  let y = [];
+  if ((array == undefined)||(array.length == y.length)){
+    return 0;
+  }
+  else {
+     let x = array[0];
+  array.forEach(item => {
+    if (item > x){
+      x = item;
+    }
+  });
+  return x;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let y = [];
+  if ((array == undefined)||(array.length == y.length)){
+    return 0;
+  }
+  else {
+     let x = array[0];
+  for (let i = 1; i < array.length; i++) {
+      x += array[i];
+  };
+  return x/array.length;
+  }
 }
